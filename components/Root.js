08ae638e3212639223ca142178ffc45353cd893f
Michael Probber <michael@probber.com>
2018-12-13 22:12:36 -0800

    New build stuff

---
+++ components/Root.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Root = () => (
+  <div>
+    <Helmet>
+      <title>Michael Probber</title>
+    </Helmet>
+    hello world
+  </div>
+);
+export default Root;
---
+++ index.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root';
+
+const root = document.createElement('div');
+if (document.body) document.body.appendChild(root);
+root.style.setProperty('height', '100%');
+ReactDOM.render(<Root />, root);
